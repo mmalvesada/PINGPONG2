f2f1d345fc1f9dfd966d62acf3c0bdf54d251b8b
Mmalvesada <mariamalvesadarodriguez@hotmail.es>
2021-10-30 12:10:20 +0200

    pingpongV1b
    Este código se bloquea por que estamos usando boolean

--- pingpong/src/pingpong/Game.java
+++ pingpong/src/pingpong/Game.java
@@ -19,21 +19,54 @@ public class Game {
     public static void main(String[] args) {
 
 
+
         Player player1 = new Player("ping");
+
         Player player2 = new Player("pong");
-        
+
+
+
         player1.setNextPlayer(player2);
+
         player2.setNextPlayer(player1);
-        
+
+
+
         System.out.println("Game starting...!");
-        
+
+
+
         player1.setMustPlay(true);
-        
+
+
+
         Thread thread2 = new Thread(player2);
+
         thread2.start();
+
         Thread thread1 = new Thread(player1);
+
         thread1.start();
-        
+
+
+
+        try {
+
+            thread1.join();
+
+            thread2.join();
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+// tenemos mas control sobre los hilos, pero no total, porque se va a 
+//quedar pillado y no va  a terminar
+
+//el must play es visible porque lo hace el propio hilo, 
+//y esto es malo porque depende del hilo,
+
         System.out.println("Game finished!");
 
     }
